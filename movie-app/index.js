338db1220ed606bd00bec56178eb7a356cc46bf5
sxtim <sxtimx@gmail.com>
2022-02-09 19:17:45 +0300

    feat: add change film name

--- movie-app/index.js
+++ movie-app/index.js
@@ -1,5 +1,5 @@
 const apiKey = '60bdc579-3b42-4708-abea-0b67e0e91fa6';
-const apiUrlPopular = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1';
+const apiUrlPopular = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
 const apiSearch = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
 
 // example api kinopoisk
@@ -34,14 +34,18 @@ async function getMovies(url) {
 }
 
 //получаем цвет рейтинга
-function getColorClassByRate (rate) {
+function getColorClassByRate(rate) {
     if (rate >= 7) {
         return 'green';
     }
     if (rate > 5) {
         return 'orange';
-    }
-    else return 'red';
+    } else return 'red';
+}
+
+//если нет названия на англ, получаем на ру
+function changeFilmName(nameEn, nameRu) {
+    return nameEn === null ? nameRu : nameEn;
 }
 
 // отрисовку карточек фильмов переносим в js
@@ -51,8 +55,16 @@ const showMovies = (data) => {
     //очищаем страницу
     document.querySelector('.movies__container').innerHTML = '';
 
+    // у блока movies__container создаем div для каждого фильма
     data.films.forEach((movie) => {
-        //у блока movies__container создаем div для каждого фильма
+        //обработка null в рейтинге
+        if (movie.rating === 'null') {
+            movie.rating = 'n/a';
+        }
+        if (movie.rating.includes('%')) {
+            movie.rating = movie.rating.slice(0, 2) / 10 ;
+        }
+
         const movieItem = document.createElement('div');
         movieItem.classList.add('movies__item');
         movieItem.innerHTML =
@@ -61,7 +73,7 @@ const showMovies = (data) => {
                      alt=${movie.nameEn} class="movie__cover">
                 <div class="movie__cover-darkened"></div>
             </div>
-            <div class="movie__title">${movie.nameEn}</div>
+            <div class="movie__title">${changeFilmName(movie.nameEn, movie.nameRu)}</div>
             <div class="movie__category">${movie.genres.map((genre) => ` ${genre.genre}`)}</div>
             <div class="movie__average movie__average--${getColorClassByRate(movie.rating)}">${movie.rating}</div>
         </div>`;
@@ -74,12 +86,11 @@ const form = document.querySelector('form');
 const search = document.querySelector('.search');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); //отменяем перезагрузку страницы при sabmit
 
     const searchUrl = `${apiSearch}${search.value}`
 
     if (search.value) {
-
         getMovies(searchUrl);
 
         // search.value = '';
